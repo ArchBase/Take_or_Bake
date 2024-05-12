8c25f2b90c61d5b5efa265645c6b9988011f3192
ArchBase <archbase557@gmail.com>
2024-05-12 10:14:17 +0530

    road boudry based ai driver. Failed

--- car/BaseCar.gd
+++ car/BaseCar.gd
@@ -1,48 +1,63 @@
 extends VehicleBody3D
 
 
-@export var STEER_SPEED = 1.5
+@export var STEER_SPEED = 2
 @export var STEER_LIMIT = 0.6
 var steer_target = 0
 @export var engine_force_value = 40
+var left = 0
+var right = 0
+var stop = false
+
+func process_AI():
+	if $left.is_colliding() and $center.is_colliding():
+		left = 0
+		right = 1
+	else:
+		right = 0
+	if $right.is_colliding() and $center.is_colliding():
+		right = 0
+		left = 1
+	else:
+		left = 0
+	if $right.is_colliding() and $left.is_colliding():
+		right = 0
+		left = 0
+	if $right.is_colliding() and $left.is_colliding() and $center.is_colliding():
+		stop = true
+	print($left.is_colliding(), $center.is_colliding(), $right.is_colliding())
 
 func _physics_process(delta):
+	process_AI()
 	var speed = linear_velocity.length()*Engine.get_frames_per_second()*delta
 	traction(speed)
 	$Hud/speed.text=str(round(speed*3.8))+"  KMPH"
 
 	var fwd_mps = transform.basis.x.x
-	steer_target = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
+	#steer_target = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
+	steer_target = left - right
+	#print(steer_target)
 	steer_target *= STEER_LIMIT
-	if Input.is_action_pressed("ui_down"):
-	# Increase engine force at low speeds to make the initial acceleration faster.
-
-		if speed < 20 and speed != 0:
-			engine_force = clamp(engine_force_value * 3 / speed, 0, 300)
-		else:
-			engine_force = engine_force_value
-	else:
+	#if Input.is_action_pressed("ui_up"):
+	#print(speed > 0.000001)
+	if speed > 3:
 		engine_force = 0
-	if Input.is_action_pressed("ui_up"):
-		# Increase engine force at low speeds to make the initial acceleration faster.
-		if fwd_mps >= -1:
-			if speed < 30 and speed != 0:
-				engine_force = -clamp(engine_force_value * 10 / speed, 0, 300)
-			else:
+	else:
+		#stop = false
+		#print("not")
+		if not stop:
+		#if Input.is_action_pressed("ui_up"):
 				engine_force = -engine_force_value
 		else:
-			brake = 1
-	else:
-		brake = 0.0
-		
-	if Input.is_action_pressed("ui_select"):
+			brake = 0.0
+			
+	#if Input.is_action_pressed("ui_select"):
+	if stop:
+		engine_force = 0
 		brake=3
-		$wheal2.wheel_friction_slip=0.8
-		$wheal3.wheel_friction_slip=0.8
-	else:
-		$wheal2.wheel_friction_slip=3
-		$wheal3.wheel_friction_slip=3
+
 	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
+	#print($center.is_colliding())
 
 
 
