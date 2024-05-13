961de927e2ab0c1b1108003aca9a5bf6b1dd908d
ArchBase <archbase557@gmail.com>
2024-05-13 20:59:29 +0530

    exported to windows, web
    web: no texture problem
    windows: no scene loading and no resource loading problem

--- loading.gd
+++ loading.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 var time = 0
+var do = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,7 +10,8 @@ func _ready():
 
 func _process(_delta):
 	time += _delta
-	if time > 1:
+	if time > 1 and do:
+		do = false
 		print("Open world")
 		get_tree().change_scene_to_file("res://world/scene/world.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
